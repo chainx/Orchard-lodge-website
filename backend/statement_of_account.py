920322c2801130f28363886f89577ec645d378bf
Michael Kiss <chainx@hotmail.co.uk>
2025-08-29 15:21:24 +0100

    Updated statement of account summary table to include comparison to previous months

--- backend/statement_of_account.py
+++ backend/statement_of_account.py
@@ -3,9 +3,11 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from datetime import datetime, date
+import pandas as pd
 from docx import Document
 from copy import deepcopy
 from shutil import make_archive
+import re
 
 import django
 django.setup()
@@ -18,26 +20,27 @@ def main():
 
 #==================================================================================================================================================================
 
-def produce_statements_of_account(recently_left=False):
-    print(datetime.now().date().strftime('%B %Y'))
-    
-    folder = settings.MEDIA_INVOICES / f"Statements of account ({datetime.now().date().strftime('%B %Y')}){' recently left' if recently_left else ''}"
+def produce_statements_of_account(recently_left=False):    
+    filename = f"Statements of Account ({datetime.now().date().strftime('%d-%m-%Y')}){' Recently Left' if recently_left else ''}"
+    folder = settings.MEDIA_INVOICES / filename
     if not recently_left:
         residents = resident.objects.filter(current=True)
     else:
-        residents = resident.objects.filter(leave_date__gt=date(2024,10,1))
+        residents = resident.objects.filter(leave_date__gt=date(2025,2,1))
+    resident_list = sorted(list(residents), key = lambda x: x.total_owed(), reverse=True)
 
     os.makedirs(folder, exist_ok=True)
     
     count = 1
-    for res in residents.order_by('last'):
+    for res in resident_list:
         if res.total_owed() != 0:
             write_statement_of_account(folder, count, res)
             count+=1
         else:
             print(res.name)
-            
     make_archive(folder, "zip", folder)
+
+    produce_summary_table(resident_list, filename, recently_left)
 
 def write_statement_of_account(folder, count, res):
 
@@ -75,6 +78,51 @@ def write_statement_of_account(folder, count, res):
         new_row.cells[2].text = 'Bank transfer' if pay.type in ['Santander', 'RBS'] else pay.type
 
     document.save(os.path.join(folder, f'{count}. {res.name} - Statement of Account.docx'))
+
+def produce_summary_table(resident_list, filename, recently_left):
+    owed_col_name = f"Total Owed ({datetime.now().date().strftime('%d %B')})"
+    if not recently_left:
+        df = pd.DataFrame([(res.name, res.total_owed()/100) for res in resident_list], columns=["Name", owed_col_name])
+    else:
+        df= pd.DataFrame([(res.name, res.total_owed()/100, res.leave_date) for res in resident_list], columns=["Name", owed_col_name, "Leave Date"])
+
+    date_stripped_filename = ' '.join(filename.split()[:-1])
+    pattern = f'^{date_stripped_filename} \\(\\d{{2}}-\\d{{2}}-\\d{{4}}\\)\\.xlsx$'
+    previous_filename = next(file for file in os.listdir(settings.MEDIA_INVOICES) if re.match(pattern, file))
+    previous_df = pd.read_excel(settings.MEDIA_INVOICES/previous_filename).iloc[:, 1:] # Removes index col
+    
+    combined_df = df.merge(previous_df, on="Name", how="left")
+    combined_df.index = combined_df.index + 1
+
+    with pd.ExcelWriter(settings.MEDIA_INVOICES/f"{filename}.xlsx", engine="xlsxwriter") as writer:
+        combined_df.to_excel(writer, sheet_name="Sheet1")
+        worksheet = writer.sheets["Sheet1"]
+
+        base_fmt   = writer.book.add_format({"font_size": 13})
+        pound_fmt  = writer.book.add_format({"num_format": "£#,##0.00", "font_size": 13})
+        green_fmt  = writer.book.add_format({"num_format": "£#,##0.00", "font_color": "green", "font_size": 13})
+
+
+        for col_idx, col in enumerate(combined_df.columns):
+            col_idx+=1
+            max_len = max(
+                combined_df[col].astype(str).map(len).max(),
+                len(str(col))
+            )
+            if pd.api.types.is_numeric_dtype(combined_df[col]):
+                worksheet.set_column(col_idx, col_idx, max_len, pound_fmt)
+
+                worksheet.conditional_format(
+                    1, col_idx, len(combined_df), col_idx,
+                    {
+                        "type": "cell",
+                        "criteria": "<=",
+                        "value": 0,
+                        "format": green_fmt,
+                    }
+                )
+            else:
+                worksheet.set_column(col_idx, col_idx, max_len, base_fmt)
 
 def copy_row_format(source_row, target_row):
     for source_cell, target_cell in zip(source_row.cells, target_row.cells):        
